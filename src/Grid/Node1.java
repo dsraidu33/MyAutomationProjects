e9e0b41ea69bdc1d900f2bb07b792f7465f549ad
dsraidu33 <subbarayudugit@gmail.com>
2017-09-23 14:29:34 -0400

    Second Commit

---
+++ src/Grid/Node1.java
@@ -0,0 +1,33 @@
+package Grid;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+public class Node1 {
+  @Test
+  public void f() throws InterruptedException {
+	  WebDriver driver;
+	  String nodeUrl;
+	  try{
+		  	nodeUrl = "http://192.168.99.100:4444/wd/hub";
+			DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+			capabilities.setBrowserName("chrome");
+			capabilities.setPlatform(Platform.LINUX);
+			driver = new RemoteWebDriver(new URL(nodeUrl), capabilities);
+	        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	        driver.get("https://www.amazon.com");
+	        Thread.sleep(5000);
+	        System.out.println("Title is " +driver.getTitle());
+	  }catch(MalformedURLException e)
+	  {
+		  e.printStackTrace();
+	  }
+  }
+}
---
+++ src/Grid/SeleniumGrid.java
@@ -0,0 +1,40 @@
+package Grid;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.junit.Assert;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class SeleniumGrid {
+	static WebDriver driver;
+	static String nodeUrl;
+
+	@BeforeTest
+	public void setup() throws MalformedURLException {
+		nodeUrl = "http://192.168.99.100:4444/wd/hub";
+		DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+		capabilities.setBrowserName("chrome");
+		capabilities.setPlatform(Platform.LINUX);
+		driver = new RemoteWebDriver(new URL(nodeUrl), capabilities);
+	}
+
+	@Test
+	public void simpleTest() throws InterruptedException {
+		driver.get("https://www.edureka.co/");
+		Assert.assertEquals("Instructor Led Online Courses with 24x7 On-Demand Support | Edureka", driver.getTitle());
+		Thread.sleep(6000);
+	}
+
+	@AfterTest
+	public void afterTest() {
+		//driver.quit();
+	}
+}
